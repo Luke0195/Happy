8094f4231cb887185c543f1ed9c6346923f6b1eb
Luke0195 <lucas201273@gmail.com>
2020-10-13 08:55:55 -0300

    chore: add script typeorm

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
-
+import './database/connection';
 const app = express();
 app.use(express.json());
 
 
 app.get('/users', (request, response) =>{
  return response.json({message: 'Welcome to the next level week #03'})
-})
+});
+
 app.listen(3333, ()=>{
   console.log(' O servidor está rodando na porta http://localhost:3333');
-})
+});
+
