298c0c9725219ecde0af4bc9ee93dd64f9654786
Luke0195 <lucas201273@gmail.com>
2020-10-13 12:08:58 -0300

    chore: add express-async-errors

--- backend/src/controller/OrphanagesController.ts
+++ backend/src/controller/OrphanagesController.ts
@@ -1,9 +1,12 @@
 import {Request, Response} from 'express';
 import {getRepository} from 'typeorm';
+import orphanageView from '../views/orphanages_view';
+
 import Orphange from '../models/Orphanage';
 
 export default{
   async create(request:Request , response: Response){
+    console.log(request.files);
     const{ 
       name, 
       latitude,
@@ -14,8 +17,15 @@ export default{
       open_on_weekends } = request.body;
   
       const orphanagesRepository = getRepository(Orphange);
+
+      const requestImages = request.files as Express.Multer.File[];
+      const images = requestImages.map(image =>{
+        return { path: image.filename}
+      })
+
+
       const orphanage = orphanagesRepository.create({ 
-        name,latitude, longitude,about, instructions, opening_hours, open_on_weekends
+        name,latitude, longitude,about, instructions, opening_hours, open_on_weekends, images
       });
   
       await orphanagesRepository.save(orphanage);
@@ -26,15 +36,15 @@ export default{
 
   async index(request:Request, response:Response){
     const orphanagesRepository = getRepository(Orphange);
-    const orphanages = await orphanagesRepository.find();
-    return response.status(200).json(orphanages);
+    const orphanages = await orphanagesRepository.find({relations:['images']});
+    return response.json(orphanageView.renderMany(orphanages));
   },
 
   async show(request:Request, response:Response){
     const { id } = request.params;
     const orphanagesRepository = getRepository(Orphange);
-    const orphanage = await orphanagesRepository.findOneOrFail(id);
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {relations:['images']});
     if(!orphanage) return response.status(400).json({message: 'Orphanage not found'});
-    return response.json(orphanage);
+    return response.json(orphanageView.render(orphanage));
   }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import {Router} from 'express';
+import multer from 'multer';
 
 import orphaneController from './controller/OrphanagesController';
+import multerConfig from './config/upload';
 const routes = Router();
+const uploads = multer(multerConfig);
 
-routes.post('/orphanages',orphaneController.create);
 routes.get('/orphanages',orphaneController.index);
 routes.get('/orphanages/:id', orphaneController.show);
+routes.post('/orphanages',uploads.array('images') ,orphaneController.create);
 
 export default routes;
